0a3cedf5e9d091b23ac140dafbafa03f3ff0f7c4
jean-low <jeanlow@al.insper.edu.br>
2020-03-09 20:04:47 -0300

    update_skill_5

--- aulas/05-localizacao/cli.py
+++ aulas/05-localizacao/cli.py
@@ -1,13 +1,18 @@
-import babel
+import babel.dates as bd
+import babel.numbers as bn
+import gettext
+
 from datetime import date
 from datetime import date, datetime, time
 
+gettext.install('cli', localedir='locale')
+
 if __name__ == '__main__':
     today = date.today()
-    print(today)
+    print(bd.format_date(today,"full"))
 
     number = 240000000000.32212
-    print(number)
+    print(bn.format_number(number))
     
-    name = input('Input your name: ')
-    print('Hello {}'.format(name))
+    name = input(_('Input your name: '))
+    print(_(f'Hello {name}')
